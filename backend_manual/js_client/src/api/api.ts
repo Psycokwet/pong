87b6905b04ce0d4ce56e8e71434dcb5deaf818ac
Sophie Carboni <socarboni@gmail.com>
2022-09-09 18:39:52 +0200

    doc, test : Adding documentation and tools to test backend

--- backend_manual/js_client/src/api/api.ts
+++ backend_manual/js_client/src/api/api.ts
@@ -4,7 +4,7 @@ import { AuthUserDto } from "@back/auth/auth-user.dto";
 type OnErrorFunction = (reason: any) => void
 type OnSuccess = (data? : object) => void
 
-export const PREFIX = 'http://localhost:3000'
+export const PREFIX = 'http://localhost:8080/api'
 export enum URL {
   CREATE_USER = '/user/',
   AUTH = '/auth/',
