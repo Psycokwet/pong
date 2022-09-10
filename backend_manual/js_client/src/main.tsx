f8c445971039dcc96d8e3dca0fe2f3a8aeedcb5c
Sophie Carboni <socarboni@gmail.com>
2022-09-10 04:13:40 +0200

    docs refactor : clean a bit js_client

--- backend_manual/js_client/src/main.tsx
+++ backend_manual/js_client/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM, { createRoot } from "react-dom/client";
 import App from "./components/App";
 import './components/index.css'
-import { Api, Bearer } from "./api/api";
 import { createUser, getAuthenticated } from "./api_call_functions";
 
 //here, mains.tsx will call both api_call_functions, you can see the detail in the webconsole
