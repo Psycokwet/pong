3173b05bd4464ef01786aa6414d3ceadbced5af8
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-12 18:56:04 +0200

    refactor, front: using map and a list instead of listing all Routers/Li
    - Add APage component
    - If we want to modify info, just change the list webPage.

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,34 +1,61 @@
-import React from 'react'
-import { NavLink, Route, Routes } from 'react-router-dom'
-import Home from './Pages-To-Change/Home'
-import Play from './Pages-To-Change/Play'
-import LeaderBoard from './Pages-To-Change/LeaderBoard'
-import Community from './Pages-To-Change/Community'
-import User from './Pages-To-Change/User'
-import NotFound from './Pages-To-Change/NotFound'
+import React from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from "./Pages-To-Change/Home";
+import Play from "./Pages-To-Change/Play";
+import LeaderBoard from "./Pages-To-Change/LeaderBoard";
+import Community from "./Pages-To-Change/Community";
+import User from "./Pages-To-Change/User";
+import NotFound from "./Pages-To-Change/NotFound";
+import APage from "./APage";
+
+const webPages = [
+  {
+    url: "/",
+    pageName: "Home",
+    element: <Home />,
+  },
+  {
+    url: "/play",
+    pageName: "play",
+    element: <Play />,
+  },
+  {
+    url: "/leaderboard",
+    pageName: "leader board",
+    element: <LeaderBoard />,
+  },
+  {
+    url: "/community",
+    pageName: "community",
+    element: <Community />,
+  },
+  {
+    url: "/user",
+    pageName: "user",
+    element: <User />,
+  },
+];
 
 const NavBar = () => {
   return (
     <div>
       <nav>
-        <ul className='hidden md:flex gap-8 p-6 uppercase bg-slate-600'>
-          <li><NavLink to="/" style={({isActive}) => ({color: isActive? "yellow": "white"})}>Pong's Game</NavLink></li>
-          <li><NavLink to="/play" style={({isActive}) => ({color: isActive? "yellow": "white"})}>Play</NavLink></li>
-          <li><NavLink to="/leaderboard" style={({isActive}) => ({color: isActive? "yellow": "white"})}>Leader Board</NavLink></li>
-          <li><NavLink to="/community" style={({isActive}) => ({color: isActive? "yellow": "white"})}>Comunity</NavLink></li>
-          <li><NavLink to="/user" style={({isActive}) => ({color: isActive? "yellow": "white"})}>User</NavLink></li>
+        <ul className="hidden md:flex gap-8 p-6 uppercase bg-slate-600">
+          {webPages.map((onePage, key) => {
+            return <APage url={onePage.url} pageName={onePage.pageName} />;
+          })}
         </ul>
       </nav>
+
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/play" element={<Play />} /> // ? component Play will be rendered when we're at site.com/play
-          <Route path="/leaderboard" element={<LeaderBoard />} />
-          <Route path="/community" element={<Community />} />
-          <Route path="/user" element={<User />} />
-          <Route path="*" element={<NotFound />} />
+        {/* Only component Play will be rendered when we're at url /play */}
+        {webPages.map((onePage, key) => {
+          return <Route path={onePage.url} element={onePage.element} />;
+        })}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
