8399b5f49dac987e55841f086cb7688d015f9f92
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-13 16:03:02 +0200

    feat, front: polish the style for NavBar
    - Page name not appears
    - Need to do mobile version (small size)

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -8,44 +8,58 @@ import User from "./Pages-To-Change/User";
 import NotFound from "./Pages-To-Change/NotFound";
 import APage from "./APage";
 
+// Icon
+import { Si42 } from 'react-icons/si'
+import { FaCog } from 'react-icons/fa'
+import { FaComments } from 'react-icons/fa'
+import { FaUser } from 'react-icons/fa'
+import { FaGamepad } from 'react-icons/fa'
+import { FaHome } from 'react-icons/fa'
+import { HiChartBar } from 'react-icons/hi'
+import { AiFillHome } from 'react-icons/ai'
+import { RiPingPongFill } from 'react-icons/ri'
+
 const webPages = [
   {
     url: "/",
     pageName: "Home",
     element: <Home />,
+    pageIcon: <AiFillHome size="28" />,
   },
   {
     url: "/play",
     pageName: "play",
     element: <Play />,
+    pageIcon: <RiPingPongFill size="32" />
   },
   {
     url: "/leaderboard",
     pageName: "leader board",
     element: <LeaderBoard />,
+    pageIcon: <HiChartBar size="28" />
   },
   {
     url: "/community",
     pageName: "community",
     element: <Community />,
+    pageIcon: <FaComments size="28" />
   },
   {
     url: "/user",
     pageName: "user",
     element: <User />,
+    pageIcon: <FaUser size="26" />
   },
 ];
 
 const NavBar = () => {
   return (
     <div>
-      <nav>
-        <ul className="hidden md:flex gap-8 p-6 uppercase bg-slate-600">
+      <div className="hidden md:flex justify-end gap-8 p-6 uppercase bg-slate-700">
           {webPages.map((onePage, key) => {
-            return <APage url={onePage.url} pageName={onePage.pageName} />;
+            return <APage url={onePage.url} pageName={onePage.pageName} pageIcon={onePage.pageIcon} />;
           })}
-        </ul>
-      </nav>
+      </div>
 
       <Routes>
         {/* Only component Play will be rendered when we're at url /play */}
