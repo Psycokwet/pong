838a3b10fb4000340de418c9bba16da7f822d0b9
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-13 16:03:02 +0200

    feat, front: polish the style for NavBar
    - Page name not appears
    - Need to do mobile version (small size)

--- frontend/src/components/NavBar/APage.tsx
+++ frontend/src/components/NavBar/APage.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { IconType } from "react-icons";
 
 type Props = {
   url: string;
   pageName: string;
+  pageIcon: IconType;
 };
-const APage: React.FC<Props> = ({ url, pageName }) => {
+
+const APage: React.FC<Props> = ({ url, pageName, pageIcon }) => {
   return (
-    <li>
-      <NavLink
+      <NavLink className="navbar-icon"
         to={url}
-        style={({ isActive }) => ({ color: isActive ? "yellow" : "white" })}
+        style={({ isActive }) => ({ color: isActive ? "green" : "white" })}
       >
-        {pageName}
+        {pageIcon}<span className="navbar-page-name">{pageName}</span>
       </NavLink>
-    </li>
   );
 };
 
