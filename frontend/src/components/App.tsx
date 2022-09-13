82c4ca117c4f959cb340034fcb0a39aed89905f3
mescande <mescande@student.42.fr>
2022-09-13 22:57:09 +0200

    WIP <frontend> : Profile page beginning
    feat profile picture component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
+import Profile from "./Profile/Profile"
 import {Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<NavBar />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   )
