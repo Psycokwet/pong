bfe46f96d38eab2f6384139e093c4ed830f19e22
Dai Clement <cdai@student.42.fr>
2022-09-14 18:36:19 +0200

    feat <front/connection>: Add connection/disconnection buttons

---
+++ frontend/src/components/ConnectionButton/ConnectionButton.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export const ConnectionButton = () => {
+  return (
+    <div>
+      <a href="http://localhost:8080/api/auth/42">
+        Connect with intra42
+      </a>
+    </div>
+  );
+}
