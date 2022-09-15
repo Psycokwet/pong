5a836423911f6e8f3dd5ddefe7fe3779679714a4
mescande <mescande@student.42.fr>
2022-09-15 18:31:46 +0200

    Fix/refactor <Frontend> : NavBar & Profile archi merge
    Going threw NavBar for Profile

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -6,7 +6,7 @@ import Home from "./Pages-To-Change/Home";
 import Play from "./Pages-To-Change/Play";
 import LeaderBoard from "./Pages-To-Change/LeaderBoard";
 import Community from "./Pages-To-Change/Community";
-import Profile from "./Pages-To-Change/Profile";
+import Profile from "./Profile/Profile";
 import Settings from "./Pages-To-Change/Settings";
 import NotFound from "./Pages-To-Change/NotFound";
 import APage from "./APage";
