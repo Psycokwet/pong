bf4593a36c03918c6d52096619db205fbd0e35ff
Sophie Carboni <socarboni@gmail.com>
2022-09-16 14:42:09 +0200

    feat : correction preparing script

---
+++ building_correction_project.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+rm -rf dist_project
+mkdir dist_project -p
+FOLDERS=( frontend backend nginx )
+
+for i in ${FOLDERS[@]}
+do
+    rsync -av ${i}/ dist_project/${i} --exclude node_modules --exclude Dockerfile.local --exclude .env --exclude '*.git*' --exclude '*.md' --exclude 'dist' --exclude 'test'
+done
+cp ./* dist_project/ 
+rm  dist_project/*.md
+rm  dist_project/`basename "$0"`
