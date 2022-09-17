d5afbb0990ed7790940c06379659600dad7b3463
NelMasr <nel-masr@e1r13p3.clusters.42paris.fr>
2022-09-17 15:20:30 +0200

    update <back>: cleaned up a bit

--- backend/src/auth/fortytwo.module.ts
+++ backend/src/auth/fortytwo.module.ts
@@ -1,26 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Game } from 'src/game/game.entity';
-import { User } from 'src/users/user.entity';
+import { User } from 'src/user/user.entity';
 import { Friend } from 'src/friend_list/friend.entity';
-import { Game } from 'src/game/game.entity';
-import { UsersModule } from 'src/users/users.module';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/user/user.module';
+import { UsersService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { FortyTwoController } from './fortytwo.controller';
 import { FortyTwoStrategy } from './fortytwo.strategy';
 import { JwtAuthModule } from './jwt.module';
 
 @Module({
-<<<<<<< HEAD
   imports: [
     JwtAuthModule,
     UsersModule,
     TypeOrmModule.forFeature([User, Game, Friend]),
   ],
-=======
-  imports: [JwtAuthModule, UsersModule, TypeOrmModule.forFeature([User, Game])],
->>>>>>> nad_route_backend
   controllers: [FortyTwoController],
   providers: [FortyTwoStrategy, UsersService, AuthService],
 })
