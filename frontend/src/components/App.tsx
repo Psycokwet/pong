9fc8fc3aa77137cee7a3f47c60c9b8feb303be9c
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-17 22:28:54 +0200

    wip, front: prettier the codes

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,7 +3,6 @@ import LoginPage from "./LoginPage/LoginPage";
 import { useState, useEffect } from "react";
 import NavBar from "./NavBar/NavBar";
 import { Api } from "../api/api";
-import PracticeJwt from "./PracticeJwt";
 import { DisconnectionButton } from "./ConnectionButton/DisconnectionButton";
 
 function App() {
@@ -17,8 +16,7 @@ function App() {
       setInterval(
         () =>
           api.refreshToken().then((res) => {
-            if (res.status !== 200)
-            {
+            if (res.status !== 200) {
               clearInterval(loopValue);
               setConnected(false);
               window.location.replace("http://localhost:8080"); //to refresh
@@ -31,16 +29,12 @@ function App() {
 
   useEffect(() => {
     api.refreshToken().then((res) => {
-      if (res.status === 200) { //status from backend
+      if (res.status === 200) {
         setConnected(true);
         connectionLoop();
       }
-      // else
-      //   window.location.replace("http://localhost:8080/api/auth/42");
-      // please replace the url on login page different than root of main page
     });
   }, []);
-
 
   return isConnected ? (
     <div>
@@ -49,8 +43,7 @@ function App() {
     </div>
   ) : (
     <LoginPage />
-  )
-  // return <PracticeJwt />
+  );
 }
 
 export default App;
