b609292b9f7307c60cad6775cc583b986b0d656d
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-18 14:12:58 +0200

    refactor, front: remove comments

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -13,25 +13,13 @@ enum connectionStatusEnum {
 const api = new Api();
 function App() {
   const [connectedState, setConnectedState] = useState(connectionStatusEnum.Unknown);
-  // const [loopValue, setLoopValue] = useState(0);
-
-
-  // const connectionLoop = () => {
-  //   setLoopValue(
-
-  //   );
-  // };
 
   useEffect(() => {
-
     if (connectedState == connectionStatusEnum.Unknown) {
       api.refreshToken().then((res) => {
         if (res.status !== 200) {
-          // clearInterval(loopValue);
           console.log("set false")
-
           setConnectedState(connectionStatusEnum.Disconnected);
-          // window.location.replace("http://localhost:8080"); //to refresh
         }
         else {
           console.log("set true")
@@ -39,34 +27,6 @@ function App() {
         }
       });
     }
-    //   // let interval: number;
-    //   // if (!isConnected) {
-    //   //   interval = setInterval(
-    //   //     () =>
-    //   api.refreshToken().then((res) => {
-    //     if (res.status !== 200) {
-    //       // clearInterval(loopValue);
-    //       console.log(isConnected, "set false")
-
-    //       setConnected(false);
-    //       window.location.replace("http://localhost:8080"); //to refresh
-    //     }
-    //     console.log(isConnected, "set true")
-    //     setConnected(true);
-    //   });
-    //   //       }),
-    //   //     5000
-    //   //   );
-    //   // }
-    //   // api.refreshToken().then((res) => {
-    //   //   if (res.status === 200) {
-    //   //     setConnected(true);
-    //   //     connectionLoop();
-    //   //   }
-    //   // return () => {
-    //   //   if (!isConnected) clearInterval(interval)
-    //   // }
-
   }, [connectedState]);
 
   return connectedState == connectionStatusEnum.Unknown ? (
