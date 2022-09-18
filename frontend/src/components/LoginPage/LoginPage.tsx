f1bd6e6675aa92d92a4aac7d22e07b9d94053970
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-18 15:32:41 +0200

    refactor, front: restyling DisconnectionButton.
    - Add it into NavBar

--- frontend/src/components/LoginPage/LoginPage.tsx
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { useState } from "react";
 
-import { Api } from "../../api/api";
-
-const api = new Api();
 const LoginPage = () => {
 
   const redirectOauth = () => {
