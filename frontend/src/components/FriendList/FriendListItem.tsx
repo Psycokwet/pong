09567d26520ca2acd43acb1e5fc7c430d8efe046
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-19 12:28:07 +0200

    feat, front: add FriendListItem and styling with over-flow list

---
+++ frontend/src/components/FriendList/FriendListItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// type userListProps = {
+//     userList: []
+// }
+
+const FriendListItem = () => {
+  return <li className="hover:bg-sky-600">FriendListItem</li>;
+};
+
+export default FriendListItem;
--- frontend/src/components/FriendList/SubFriendList.tsx
+++ frontend/src/components/FriendList/SubFriendList.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import FriendListItem from "./FriendListItem";
 
 type SubFriendListProps = {
-  type: string;
+  listType: string;
+  // user: [];
 };
 
-const SubFriendList = ({ type }) => {
-  return <div>{type}</div>;
+const SubFriendList: React.FC = ({ listType }) => {
+  return (
+    <div>
+      <div>{listType}</div>
+      <ul>
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+      </ul>
+    </div>
+  );
 };
 
 export default SubFriendList;
