f03735ccf3f2c86bc9ab8c6d7e76eaaf1bb7d566
Dai Clement <cdai@student.42.fr>
2022-09-19 13:05:07 +0200

    feat [front][websocket_for_chat]: add feat to join a room

--- frontend/src/components/WebSocketCdaiTest/JoinRoomButton.tsx
+++ frontend/src/components/WebSocketCdaiTest/JoinRoomButton.tsx
@@ -4,21 +4,49 @@ import MessageInput from './MessageInput';
 import Messages from './Messages';
 const ENDPOINT = "http://localhost:8080";
 
+interface ChannelData {
+  channelName: string;
+  channelId: number;
+}
+
 export default function JoinRoomButton(
   {
     socket,
+    allChannel,
+    setConnectedChannel,
   }:
   {
     socket: Socket | undefined,
+    allChannel: ChannelData[],
+    setConnectedChannel: any,
   }
   ) {
-    const handleClick = () => {
-      socket?.emit("joinRoom");
-    }
+
+  // console.log(allChannel)
+  const handleClick = (channelId: number) => {
+    socket?.emit("joinRoom", channelId);
+  }
+
+  const joinRoom = (message: []) => {
+    console.log(message)
+    setConnectedChannel(message)
+  }
+  useEffect(()=> {
+    socket?.on("joinedRoom", joinRoom);
+    return () => {
+      socket?.off("joinedRoom", joinRoom);
+    };
+  }, [joinRoom]);
 
   return (
-    <div>
-      <button onClick={handleClick}>JOIN TEST</button>
+    <div> 
+      {
+        allChannel.map((channel) =>
+        <div>
+          <button onClick={() => handleClick(channel.channelId)}>Join {channel.channelName}</button>
+        </div>
+        )
+      }
     </div>
   )
 }
