91e028095a274353eff7706165bf947d80e96edb
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-19 17:14:38 +0200

    update, front, FriendList: add status of user but it might not a good idea

---
+++ frontend/src/components/FriendList/DropDownFriendList.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DropDownFriendList = () => {
+  return (
+
+}
+
+export default DropDownFriendList
--- frontend/src/components/FriendList/FriendList.tsx
+++ frontend/src/components/FriendList/FriendList.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import SubFriendList from "./SubFriendList";
 import { BiChevronDown } from "react-icons/bi";
-import { AiOutlineSearch } from "react-icons/ai";
+import { FaSearch } from "react-icons/fa";
+import { RiRadioButtonLine } from "react-icons/ri"
+import DropDownFriendList from "./DropDownFriendList";
+
+/***************** TYPE ***************************************/
+
+enum userStatusEnum {
+  Online,
+  Playing,
+  Offline,
+}
 
 /**************************************************
  * I guess this data we'll get from backend /
@@ -12,60 +21,52 @@ const UserFriendList = [
   {
     login: "scarboni",
     nickname: "scarboni",
-    status: "online",
+    status: userStatusEnum.Online,
     link_to_profile: "/scarboni",
   },
   {
     login: "scarboni1",
     nickname: "scarboni1",
-    status: "online",
+    status: userStatusEnum.Online,
     link_to_profile: "/scarboni1",
   },
   {
     login: "cdai",
     nickname: "cdai",
-    status: "offline",
+    status: userStatusEnum.Offline,
     link_to_profile: "/cdai",
   },
   {
     login: "cdai1",
     nickname: "cdai1",
-    status: "offline",
+    status: userStatusEnum.Offline,
     link_to_profile: "/cdai1",
   },
   {
     login: "nader",
     nickname: "nader",
-    status: "playing",
+    status: userStatusEnum.Playing,
     link_to_profile: "/nader",
   },
   {
     login: "nader1",
     nickname: "nader1",
-    status: "playing",
+    status: userStatusEnum.Playing,
     link_to_profile: "/nader1",
   },
   {
     login: "moot",
     nickname: "moot",
-    status: "offline",
+    status: userStatusEnum.Offline,
     link_to_profile: "/moot",
   },
   {
     login: "moot1",
     nickname: "moot1",
-    status: "offline",
+    status: userStatusEnum.Offline,
     link_to_profile: "/moot1",
   },
 ];
-
-/***************** TYPE ***************************************/
-
-enum userStatusEnum {
-  Online,
-  Playing,
-  Offline,
-}
 
 /***************** Component **********************************/
 
@@ -78,13 +79,13 @@ const FriendList = () => {
   };
 
   const goToUserPage = () => {
-    window.location.replace("http://localhost:8080/profile")
+    window.location.replace("http://localhost:8080/profile");
   };
 
   return (
-    <div className="text-white bg-gray-900 w-60">
+    <div className="text-white bg-gray-900">
       <div
-        className="w-full p-2 flex items-center justify-between text-2xl font-bold"
+        className="p-2 flex justify-between items-center text-2xl font-bold"
         onClick={showList}
       >
         Friend List
@@ -92,34 +93,38 @@ const FriendList = () => {
       </div>
       <div
         className={
-          active ? "bg-gray-700 mt-2 max-h-40 overflow-y-auto" : "hidden"
+          active ? "bg-gray-700 mt-2 max-h-60 overflow-y-auto" : "hidden"
         }
       >
-        <div className="flex items-center px-2 sticky top-0 bg-white">
-          <AiOutlineSearch size={18} className="text-gray-700" />
+        <div className="flex items-center p-1 bg-white">
+          <span>
+            <FaSearch size={20} className="text-gray-500" />
+          </span>
           <input
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
             placeholder="Search a name"
-            className="text-gray-700 placeholder:text-gray-700 p-2 outline-none"
+            className="text-gray-700 placeholder:text-gray-500 p-2 outline-none"
           />
         </div>
         {UserFriendList?.map((friend) => (
           <li
             key={friend.nickname}
-            className={`p-2 hover:bg-sky-600 hover:text-white
+            className={`flex justify-between p-2 hover:bg-sky-600 hover:text-white
             ${
-              friend.nickname.toLowerCase().startsWith(inputValue)
+              friend.nickname.startsWith(inputValue)
                 ? "block"
                 : "hidden"
             }`}
             onClick={goToUserPage}
           >
-            {friend.nickname}
-            <span>{friend.status}</span>
+            <span>{friend.nickname}</span>
+            <span className={`${friend.status === userStatusEnum.Offline ? "text-red-600" : "text-green-600"}`}
+            ><RiRadioButtonLine size="15"/></span>
           </li>
         ))}
+      {/* <DropDownFriendList /> */}
       </div>
     </div>
   );
