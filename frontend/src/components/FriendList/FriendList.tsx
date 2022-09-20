cc841c18ff151589964ec6079c4138d4a32359a7
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-20 11:27:55 +0200

    feature, friendlist, front: add buttons to Friendlists (message, watch, challenge)

--- frontend/src/components/FriendList/FriendList.tsx
+++ frontend/src/components/FriendList/FriendList.tsx
@@ -15,7 +15,7 @@ export enum userStatusEnum {
 }
 
 /**************************************************
- * I guess this is the kind of data we'll get from 
+ * I guess this is the kind of data we'll get from
  * backend / database through api ?! To DELETE later.
  **************************************************/
 const UserFriendList = [
@@ -69,22 +69,44 @@ const UserFriendList = [
   },
 ];
 
+/******************* Challenge / Watch Stream ************
+ * To be changed later...
+ *********************************************************/
+
+const inviteToChallenge = () => {
+  console.log("Wanna play?");
+};
+
+const watchStream = () => {
+  console.log("Streaming...");
+};
+
+const sendMessage = () => {
+  console.log("Send message: Hi there!!!");
+};
+
 /******************* Sub List ************************/
 const subList = [
   {
     status: userStatusEnum.Online,
-    color: "text-green-400",
+    color: "green-400",
     group_name: "Online",
+    invite: inviteToChallenge,
+    button_name: "Challenge",
   },
   {
     status: userStatusEnum.Playing,
-    color: "text-yellow-400",
+    color: "yellow-400",
     group_name: "Playing",
+    invite: watchStream,
+    button_name: "Watch",
   },
   {
     status: userStatusEnum.Offline,
-    color: "text-red-500",
+    color: "red-500",
     group_name: "Offline",
+    invite: sendMessage,
+    button_name: "Message",
   },
 ];
 
@@ -99,47 +121,49 @@ const FriendList = () => {
   };
 
   return (
-      <div className="absolute top-[120px] right-0 text-white bg-gray-900">
-        <div
-          className="p-2 flex items-center text-2xl font-bold"
-          onClick={showList}
-        >
-          <span className="px-4">Friend List</span>
-          <BiChevronDown size={20} className={`${active && "rotate-180"}`} />
+    <div className="absolute top-[120px] right-0 text-white bg-gray-900">
+      <div
+        className="p-2 flex items-center text-2xl font-bold"
+        onClick={showList}
+      >
+        <span className="px-4">Friend List</span>
+        <BiChevronDown size={20} className={`${active && "rotate-180"}`} />
+      </div>
+      <div
+        className={
+          active ? "bg-gray-700 mt-2 max-h-60 overflow-y-auto" : "hidden"
+        }
+      >
+        <div className="flex items-center p-1 bg-white">
+          <span>
+            <FaSearch size={20} className="text-gray-500" />
+          </span>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Search a name"
+            className="text-gray-700 placeholder:text-gray-500 p-2 outline-none rounded-xl bg-sky-500"
+          />
         </div>
-        <div
-          className={
-            active ? "bg-gray-700 mt-2 max-h-60 overflow-y-auto" : "hidden"
-          }
-        >
-          <div className="flex items-center p-1 bg-white">
-            <span>
-              <FaSearch size={20} className="text-gray-500" />
-            </span>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              placeholder="Search a name"
-              className="text-gray-700 placeholder:text-gray-500 p-2 outline-none rounded-xl w-40"
-            />
-          </div>
-          <div>
-            {subList?.map((aSubList) => {
-              return (
-                <SubFriendList
-                  key={aSubList.status}
-                  friend_list={UserFriendList}
-                  input={inputValue}
-                  user_status={aSubList.status}
-                  color={aSubList.color}
-                  group_name={aSubList.group_name}
-                />
-              );
-            })}
-          </div>
+        <div>
+          {subList?.map((aSubList) => {
+            return (
+              <SubFriendList
+                key={aSubList.status}
+                friend_list={UserFriendList}
+                input={inputValue}
+                group_status={aSubList.status}
+                color={aSubList.color}
+                group_name={aSubList.group_name}
+                handle_click={aSubList.invite}
+                group_button_name={aSubList.button_name}
+              />
+            );
+          })}
         </div>
       </div>
+    </div>
   );
 };
 
