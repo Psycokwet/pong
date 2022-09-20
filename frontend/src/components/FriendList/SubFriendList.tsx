71dc559bf741481af34bfacf0f4121bce4f816a7
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-20 11:27:55 +0200

    feature, friendlist, front: add buttons to Friendlists (message, watch, challenge)

--- frontend/src/components/FriendList/SubFriendList.tsx
+++ frontend/src/components/FriendList/SubFriendList.tsx
@@ -10,17 +10,21 @@ type SubFriendListProps = {
     link_to_profile: string;
   }[];
   input: string;
-  user_status: userStatusEnum;
+  group_status: userStatusEnum;
   color: string;
   group_name: string;
+  handle_click: React.MouseEventHandler<HTMLButtonElement>;
+  group_button_name: string;
 };
 
 const SubFriendList: React.FC<SubFriendListProps> = ({
   friend_list,
   input,
-  user_status,
+  group_status,
   color,
   group_name,
+  handle_click,
+  group_button_name,
 }) => {
   /******* Url will be changed later ************************/
   let imageURL = "https://picsum.photos/400";
@@ -31,28 +35,38 @@ const SubFriendList: React.FC<SubFriendListProps> = ({
   /***********************************************************/
 
   return (
-    <div className="w-40">
+    <>
       <div className="flex items-center font-bold">
-        <span className="text-green-600 px-2">
-          <BsCircleFill size="15" className={` ${color}`} />
+        <span className="px-2">
+          <BsCircleFill size="15" className={`text-${color}`} />
         </span>
         {group_name}
       </div>
       {friend_list?.map((friend) => {
-        if (friend.status === user_status)
+        if (friend.status === group_status)
           return (
-            <li
+            <div
               key={friend.login}
-              className={`flex items-center p-2 indent-2 hover:bg-sky-600 hover:text-white
+              className={`grid grid-rows-3 grid-flow-col mx-2 cursor-pointer
               ${friend.nickname.startsWith(input) ? "block" : "hidden"}`}
-              onClick={goToUserPage}
             >
-              <img src={imageURL} alt="Avatar" className="w-8 rounded-3xl" />
-              <strong>{friend.nickname}</strong>
-            </li>
+              <div
+                className="row-span-2 px-4"
+                onClick={goToUserPage}
+              >
+                <img src={imageURL} alt="Avatar" className="w-10 rounded-3xl" />
+              </div>
+              <span className="row-span-2 py-2">{friend.nickname}</span>
+              <button
+                className={`row-span-2 rounded-xl bg-gray-600 hover:bg-gray-800 text-${color}`}
+                onClick={handle_click}
+              >
+                {group_button_name}
+              </button>
+            </div>
           );
       })}
-    </div>
+    </>
   );
 };
 
