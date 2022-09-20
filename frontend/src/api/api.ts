bd6237aec9cd896c305a8643143b9a4486df93e7
Sophie Carboni <socarboni@gmail.com>
2022-09-20 11:41:32 +0200

    fix, feat : fix back get request get_friend_list + add test req on front

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,68 +1,87 @@
 import { AuthUserDto, AuthUserIdDto } from "./auth-user.dto";
 
-type OnErrorFunction = (reason: any) => void
-type OnSuccess = (data?: object) => void
+type OnErrorFunction = (reason: any) => void;
+type OnSuccess = (data?: object) => void;
 
-export const PREFIX = import.meta.env.VITE_CONTEXT == "MOCKUP" ? 'http://localhost:8080/api_mockup' : 'http://localhost:8080/api'
+export const PREFIX =
+  import.meta.env.VITE_CONTEXT == "MOCKUP"
+    ? "http://localhost:8080/api_mockup"
+    : "http://localhost:8080/api";
 export enum URL {
-  CREATE_USER = '/user/',
-  AUTH = '/auth/42/',
-  LOGOUT = '/auth/42/logout',
-  HELLO = '/',
-  PROTECTED = '/protected',
-  REFRESH_TOKEN = '/auth/refresh',
-  SIGNOUT = '/user/signout',
-  SET_PICTURE = '/user/set_picture',
-  GET_PICTURE = '/user/get_picture',
+  CREATE_USER = "/user/",
+  AUTH = "/auth/42/",
+  LOGOUT = "/auth/42/logout",
+  HELLO = "/",
+  PROTECTED = "/protected",
+  REFRESH_TOKEN = "/auth/refresh",
+  SIGNOUT = "/user/signout",
+  SET_PICTURE = "/user/set_picture",
+  GET_PICTURE = "/user/get_picture",
+  GET_FRIEND_LIST = "/user/get_friends_list",
 }
 export enum HeadersFields {
-  ContentType = 'Content-Type',
-  Authorization = 'Authorization',
+  ContentType = "Content-Type",
+  Authorization = "Authorization",
 }
 
 export type Bearer = {
-  access_token: string
-}
+  access_token: string;
+};
 
 export class Api {
-  private static readonly header = new Headers()
+  private static readonly header = new Headers();
 
   static {
-    Api.header.append(HeadersFields.ContentType, "application/json")
+    Api.header.append(HeadersFields.ContentType, "application/json");
   }
 
-  private readonly _headers = new Headers()
+  private readonly _headers = new Headers();
 
   constructor() {}
 
   setToken(token: string) {
-    this._headers.set(HeadersFields.Authorization, `Bearer ${token}`)
+    this._headers.set(HeadersFields.Authorization, `Bearer ${token}`);
   }
 
   private static async fetch(url: string, data: any): Promise<any> {
-    return fetch(url, {method: 'POST', body: data, headers: this.header})
+    return fetch(url, { method: "POST", body: data, headers: this.header });
   }
 
-  private static fetchNoResponseBody(url: string, data: object, onSuccess: OnSuccess, onError: OnErrorFunction) {
-    const myVar = fetch(url, { method: 'POST', body: JSON.stringify(data), headers: this.header })
-      .then(r => {
+  private static fetchNoResponseBody(
+    url: string,
+    data: object,
+    onSuccess: OnSuccess,
+    onError: OnErrorFunction
+  ) {
+    const myVar = fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: this.header,
+    })
+      .then((r) => {
         if (r.status !== 201) {
-          r.json().then(d => {
+          r.json().then((d) => {
             throw d;
-          })
-          return
+          });
+          return;
         }
-        onSuccess()
+        onSuccess();
       })
-      .catch(onError)
+      .catch(onError);
   }
 
   hello() {
-    return fetch(`${PREFIX}${URL.HELLO}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${URL.HELLO}`, {
+      method: "GET",
+      headers: this._headers,
+    });
   }
 
   logout() {
-    return fetch(`${PREFIX}${URL.LOGOUT}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${URL.LOGOUT}`, {
+      method: "GET",
+      headers: this._headers,
+    });
   }
   //cors not the way to go
   // login() {
@@ -70,14 +89,53 @@ export class Api {
   // }
 
   setPicture(data: FormData) {
-    return fetch(`${PREFIX}${URL.SET_PICTURE}`, {method: 'POST', body: data})
+    return fetch(`${PREFIX}${URL.SET_PICTURE}`, { method: "POST", body: data });
   }
 
   getPicture() {
-    return fetch(`${PREFIX}${URL.GET_PICTURE}`, {method: 'GET', headers: this._headers})
+    return fetch(`${PREFIX}${URL.GET_PICTURE}`, {
+      method: "GET",
+      headers: this._headers,
+    });
   }
 
   refreshToken() {
-    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, {
+      method: "GET",
+      headers: this._headers,
+    });
+  }
+  add_friend() {
+    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, {
+      method: "GET",
+      headers: this._headers,
+    });
+  }
+
+  get_friend_list(login42: string) {
+    return fetch(
+      `${PREFIX}${URL.GET_FRIEND_LIST}` +
+        "?" +
+        new URLSearchParams({ login42 }),
+      {
+        method: "GET",
+        headers: this._headers,
+      }
+    );
+  }
+
+  get_nickname(login42: string) {
+    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, {
+      method: "GET",
+      headers: this._headers,
+      body: login42,
+    });
+  }
+  set_nickname(newNicknamePong: string) {
+    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, {
+      method: "POST",
+      headers: this._headers,
+      body: newNicknamePong,
+    });
   }
 }
--- frontend/src/components/NavBar/Pages-To-Change/LeaderBoard.tsx
+++ frontend/src/components/NavBar/Pages-To-Change/LeaderBoard.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Api } from "../../../api/api";
 
+const api = new Api();
 const LeaderBoard = () => {
+  const sendRequest = () => {
+    api.get_friend_list("scarboni").then((res: Response) => {
+      console.log(res);
+      res.json().then((content) => {
+        console.log("get_friend_list", content);
+      });
+    });
+  };
   return (
-    <h1 className='text-amber-600'>I'm LeaderBoard page</h1>
-  )
-}
+    <>
+      <h1 className="text-amber-600">I'm practice request page</h1>
+      <button
+        className="bg-sky-500 hover:bg-sky-700 text-3xl rounded-3xl p-4 shadow-md shadow-blue-500/50"
+        onClick={sendRequest}
+      >
+        send request
+      </button>
+    </>
+  );
+};
 
-export default LeaderBoard
+export default LeaderBoard;
