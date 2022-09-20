277b2825f25973440ba41536acde5b22af6b4f32
mescande <matthieu.escande@gmail.com>
2022-09-20 11:31:06 +0200

    Feat <frontend> : Setup Navbar size for pages integration
    Navbar : 1/8
    New Tailwind component

--- frontend/src/components/Community/Community.tsx
+++ frontend/src/components/Community/Community.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 function Community () {
 	return (
-		<div className="bg-black text-white h-screen flex grid grid-cols-5 grid-rows-6 gap-4">
+		<div className="bg-black text-white h-7/8 flex grid grid-cols-5 grid-rows-6 gap-4">
 			<div className="h-full row-start-1 row-span-6 col-start-1 self-center scroll-smooth overflow-y-auto overflow-scroll scroll-pb-96 snap-y snap-end">
 				<div className="relative">
 					<div className="sticky top-0 px-4 py-3 flex items-center font-semibold text-sm text-slate-900 dark:text-slate-200 bg-slate-50/90 dark:bg-slate-700/90 backdrop-blur-sm ring-1 ring-slate-900/10 dark:ring-black/10">Channels</div>
@@ -37,7 +37,7 @@ function Community () {
 				<UserPicture />
 				<UserPicture />
 			</div>
-			<div className="row-span-1 row-start-9 col-span-3 col-start-2">
+			<div className="row-span-1 row-start-9 col-span-3 col-start-2 overflow-hidden">
 				<UserPicture />
 			</div>
 			<div className="row-start-1 row-span-6 col-start-5">
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+		height: {
+			'1/8': '12.5%',
+			'3/8': '37.5%',
+			'5/8': '62.5%',
+			'7/8': '87.5%',
+		},
+	},
   },
   plugins: [],
 }
