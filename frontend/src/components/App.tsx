1e9be41ed24350fb519c3c3d4291085762bcc364
Sophie Carboni <socarboni@gmail.com>
2022-09-20 15:26:07 +0200

    feat : adding shared routes consts

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -8,19 +8,21 @@ import { Loading } from "./Common/Loading";
 enum connectionStatusEnum {
   Unknown,
   Connected,
-  Disconnected
+  Disconnected,
 }
 const api = new Api();
 function App() {
-  const [connectedState, setConnectedState] = useState(connectionStatusEnum.Unknown);
+  const [connectedState, setConnectedState] = useState(
+    connectionStatusEnum.Unknown
+  );
 
   useEffect(() => {
     if (connectedState == connectionStatusEnum.Unknown) {
       api.refreshToken().then((res) => {
         if (res.status !== 200) {
+          console.log(res);
           setConnectedState(connectionStatusEnum.Disconnected);
-        }
-        else {
+        } else {
           setConnectedState(connectionStatusEnum.Connected);
         }
       });
@@ -29,13 +31,16 @@ function App() {
 
   return connectedState == connectionStatusEnum.Unknown ? (
     <Loading></Loading>
-  ) : (connectedState == connectionStatusEnum.Connected ? (
+  ) : connectedState == connectionStatusEnum.Connected ? (
     <div>
-      <NavBar setDisconnected={() => setConnectedState(connectionStatusEnum.Disconnected)}/>
+      <NavBar
+        setDisconnected={() =>
+          setConnectedState(connectionStatusEnum.Disconnected)
+        }
+      />
     </div>
   ) : (
     <LoginPage />
-  )
   );
 }
 
