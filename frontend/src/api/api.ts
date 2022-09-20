9e8b60e998c0e15f8148e83e1aabb57d55aa2d8f
Sophie Carboni <socarboni@gmail.com>
2022-09-20 15:26:07 +0200

    feat : adding shared routes consts

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,85 +1,102 @@
-import { AuthUserDto, AuthUserIdDto } from "./auth-user.dto";
+import { FULL_ROUTE } from "../../shared/routes";
+// type OnErrorFunction = (reason: any) => void;
+// type OnSuccess = (data?: object) => void;
 
-type OnErrorFunction = (reason: any) => void
-type OnSuccess = (data?: object) => void
-
-export const PREFIX = import.meta.env.VITE_CONTEXT == "MOCKUP" ? 'http://localhost:8080/api_mockup' : 'http://localhost:8080/api'
-export enum URL {
-  CREATE_USER = '/user/',
-  AUTH = '/auth/42/',
-  LOGOUT = '/auth/42/logout',
-  HELLO = '/',
-  PROTECTED = '/protected',
-  REFRESH_TOKEN = '/auth/refresh',
-  SIGNOUT = '/user/signout',
-  SET_PICTURE = '/user/set_picture',
-  GET_PICTURE = '/user/get_picture',
-}
+export const PREFIX =
+  import.meta.env.VITE_CONTEXT == "MOCKUP"
+    ? "http://localhost:8080/api_mockup"
+    : "http://localhost:8080/api";
+// export enum URL {
+//   // LOGOUT = "/auth/42/logout",
+//   HELLO = "/",
+//   PROTECTED = "/protected",
+//   // REFRESH_TOKEN = "/auth/refresh",
+//   // SIGNOUT = "/user/signout",
+//   // SET_PICTURE = "/user/set_picture",
+//   // GET_PICTURE = "/user/get_picture",
+// }
 export enum HeadersFields {
-  ContentType = 'Content-Type',
-  Authorization = 'Authorization',
+  ContentType = "Content-Type",
+  Authorization = "Authorization",
 }
 
-export type Bearer = {
-  access_token: string
-}
+// export type Bearer = {
+//   access_token: string;
+// };
 
 export class Api {
-  private static readonly header = new Headers()
+  // private static readonly header = new Headers();
 
-  static {
-    Api.header.append(HeadersFields.ContentType, "application/json")
-  }
+  // static {
+  //   Api.header.append(HeadersFields.ContentType, "application/json");
+  // }
 
-  private readonly _headers = new Headers()
+  // private readonly _headers = new Headers();
 
-  constructor() {}
+  // constructor() {}
 
-  setToken(token: string) {
-    this._headers.set(HeadersFields.Authorization, `Bearer ${token}`)
-  }
+  // setToken(token: string) {
+  //   this._headers.set(HeadersFields.Authorization, `Bearer ${token}`);
+  // }
 
-  private static async fetch(url: string, data: any): Promise<any> {
-    return fetch(url, {method: 'POST', body: data, headers: this.header})
-  }
+  // private static async fetch(url: string, data: any): Promise<any> {
+  //   return fetch(url, { method: "POST", body: data, headers: this.header });
+  // }
 
-  private static fetchNoResponseBody(url: string, data: object, onSuccess: OnSuccess, onError: OnErrorFunction) {
-    const myVar = fetch(url, { method: 'POST', body: JSON.stringify(data), headers: this.header })
-      .then(r => {
-        if (r.status !== 201) {
-          r.json().then(d => {
-            throw d;
-          })
-          return
-        }
-        onSuccess()
-      })
-      .catch(onError)
-  }
+  // private static fetchNoResponseBody(
+  //   url: string,
+  //   data: object,
+  //   onSuccess: OnSuccess,
+  //   onError: OnErrorFunction
+  // ) {
+  //   const myVar = fetch(url, {
+  //     method: "POST",
+  //     body: JSON.stringify(data),
+  //     headers: this.header,
+  //   })
+  //     .then((r) => {
+  //       if (r.status !== 201) {
+  //         r.json().then((d) => {
+  //           throw d;
+  //         });
+  //         return;
+  //       }
+  //       onSuccess();
+  //     })
+  //     .catch(onError);
+  // }
 
   hello() {
-    return fetch(`${PREFIX}${URL.HELLO}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${FULL_ROUTE.ROOT.ENDPOINT}`, {
+      method: "GET",
+      // headers: this._headers,
+    });
   }
 
   logout() {
-    return fetch(`${PREFIX}${URL.LOGOUT}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${FULL_ROUTE.AUTH.LOGOUT}`, {
+      method: "GET",
+      // headers: this._headers,
+    });
   }
-  //cors not the way to go
-  // login() {
-  //   return fetch(`${PREFIX}${URL.AUTH}`, { method: 'GET', headers: this._headers })
-  // }
-
   setPicture(data: FormData) {
-    return fetch(`${PREFIX}${URL.SET_PICTURE}`, {method: 'POST', body: data})
+    return fetch(`${PREFIX}${FULL_ROUTE.USER.SET_PICTURE}`, {
+      method: "POST",
+      body: data,
+    });
   }
 
   getPicture() {
-    return fetch(`${PREFIX}${URL.GET_PICTURE}`, {method: 'GET', headers: this._headers})
+    return fetch(`${PREFIX}${FULL_ROUTE.USER.GET_PICTURE}`, {
+      method: "GET",
+      // headers: this._headers,
+    });
   }
 
   refreshToken() {
-    return fetch(`${PREFIX}${URL.REFRESH_TOKEN}`, { method: 'GET', headers: this._headers })
+    return fetch(`${PREFIX}${FULL_ROUTE.AUTH.REFRESH}`, {
+      method: "GET",
+      // headers: this._headers,
+    });
   }
 }
-
-
