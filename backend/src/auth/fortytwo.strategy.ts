29a06fce63bdc90aee435c4e3fbb6f537ccc343b
Dai Clement <cdai@student.42.fr>
2022-09-21 17:34:17 +0200

    clean [general][shared folder]: remove useless element + cleaning

--- backend/src/auth/fortytwo.strategy.ts
+++ backend/src/auth/fortytwo.strategy.ts
@@ -15,12 +15,7 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
   async validate(accessToken: string, refreshToken: string, profile: Profile) {
     // Here a custom User object is returned. In the the repo I'm using a UsersService with repository pattern, learn more here: https://docs.nestjs.com/techniques/database
     return {
-      user: {
-        provider: '42',
-        userId: profile.id,
-        login42: profile.username,
-        email: profile.emails[0].value,
-      },
+      profile,
       authInfo: {
         accessToken,
         refreshToken,
