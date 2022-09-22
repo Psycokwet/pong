0f1eb6ad65c993409f4a2ff5bc4f62ddd51d3e0c
NelMasr <nel-masr@e1r13p3.clusters.42paris.fr>
2022-09-22 19:59:19 +0200

    feat <back> <chat>: starting on DMs

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -5,7 +5,7 @@ import { parse } from 'cookie';
 import { WsException } from '@nestjs/websockets';
 import { InjectRepository } from '@nestjs/typeorm';
 import Message from './message.entity';
-import { Repository } from 'typeorm';
+import { FindOptionsRelations, Repository } from 'typeorm';
 import { User } from 'src/user/user.entity';
 import Room from './room.entity';
 import { UsersService } from 'src/user/user.service';
@@ -36,16 +36,10 @@ export class ChatService {
     );
   }
 
-  public async getRoomById(id: number) {
-    return this.roomsRepository.findOneBy({ id });
-  }
-
-  public async getRoomByIdWithRelations(id: number) {
+  public async getRoomsById(id: number, options?: FindOptionsRelations<Room>) {
     return this.roomsRepository.findOne({
       where: { id },
-      relations: {
-        members: true,
-      },
+      relations: options,
     });
   }
 
@@ -63,9 +57,22 @@ export class ChatService {
     return newRoom;
   }
 
+  async saveDMRoom(friendId: number, userId: number) {
+    const friend = await this.userService.getById(friendId);
+    const roomName = 'DM_' + uuidv4();
+
+    const newRoom = await Room.create({
+      roomName: `channel:${roomName}:${uuidv4()}`,
+      channelName: uuidv4(),
+      isDM: true,
+    });
+
+    await newRoom.save();
+    return newRoom;
+  }
+
   async addMemberToChannel(userId: number, room: Room) {
     const newMember = await this.userService.getById(userId);
-
     if (
       !room.members.filter(
         (member: User) => member.login42 === newMember.login42,
@@ -76,10 +83,11 @@ export class ChatService {
     room.save();
   }
 
-  async saveMessage(content: string, author: User) {
+  async saveMessage(content: string, author: User, channel: Room) {
     const newMessage = await this.messagesRepository.create({
-      content,
-      author,
+      content: content,
+      author: author,
+      room: channel,
     });
     await this.messagesRepository.save(newMessage);
     return newMessage;
