1f0810f6958efd07e6ef42f35fd51c9bd17a48da
Dai Clement <cdai@student.42.fr>
2022-09-22 16:48:03 +0200

    feat [front][game]: handle mouse move and display it

--- frontend/src/components/NavBar/Pages-To-Change/Play.tsx
+++ frontend/src/components/NavBar/Pages-To-Change/Play.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import GameCanvas from '../../PongGame/GameCanvas'
 
 const Play = () => {
   return (
-    <h1 className='text-violet-600'>I'm Play page</h1>
+    <>
+      <h1 className='text-violet-600'>I'm Play page</h1>
+      <GameCanvas/>
+    </>
   )
 }
 
