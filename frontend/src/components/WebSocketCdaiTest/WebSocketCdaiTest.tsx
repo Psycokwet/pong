a697c9b7c3631b456f2524003c4c0774dce33258
Dai Clement <cdai@student.42.fr>
2022-09-22 14:00:37 +0200

    wording [front][chat: chanAttachedUsers]: rename file
    ConnectedUserList -> ChannelAttachedUsers

--- frontend/src/components/WebSocketCdaiTest/WebSocketCdaiTest.tsx
+++ frontend/src/components/WebSocketCdaiTest/WebSocketCdaiTest.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import io, { Socket } from "socket.io-client";
-import ConnectedUserList from "./ConnectedUserList";
+import ChannelAttachedUsers from "./ChannelAttachedUsers";
 import CreateChannel from "./CreateChannel";
 import JoinChannelButtons from "./JoinChannelButtons";
 import LeaveChannelButton from "./LeaveChannelButton";
