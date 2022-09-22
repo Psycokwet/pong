22aca929cfc3f1e09ffe90f40f1ef66bd6abb7dc
mescande <mescande@student.42.fr>
2022-09-22 12:08:11 +0200

    Refactor <frontend, chat> : components separation & rename
    Rename from Community to Chat
    Code separation

---
+++ frontend/src/components/Chat/Chat.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import UserPicture from "../User Picture/UserPicture";
+import ChatList from "./ChatList/ChatList";
+//import { userStatusEnum } from "../FriendList";
+import TextField from "./Text Field/TextField";
+import Messages from "./Messages/Messages";
+
+type userType = {
+	login: string;
+	nickname: string;
+//	status: userStatusEnum;
+	link_to_profile: string;
+}
+type MessageType = {
+	content: string;
+	sender: userType;
+}
+
+function Chat () {
+	const user:userType = {login:'Moot', nickname:'mescande', link_to_profile:'Profile'};
+	const [messages, setMessages] = useState<MessageType[]>(
+	() => {
+		let tmp:MessageType[] = []
+		let i:number = 0
+		while (i < 10) {
+			tmp.push({content:i.toString(), sender:user})
+			i++;
+		}
+		return tmp
+		});
+
+	const addMessage = (newElem:MessageType) => {
+		// let condition:boolean = messages.scrollTop + messages.clientHeight === messages.scrollHeight;
+
+		setMessages((current_messages)=>{
+		return [...current_messages, newElem];
+		})
+	}
+
+	console.log(messages);
+	return (
+		<div className="bg-black text-white h-7/8 flex grid grid-cols-5 grid-rows-6 gap-4">
+			<ChatList />
+			<Messages messages={messages}/>
+			<TextField addMessage={addMessage} />
+			<div className="row-start-1 row-span-6 col-start-5">
+				<UserPicture />
+			</div>
+		</div>
+	)
+}
+
+export default Chat
