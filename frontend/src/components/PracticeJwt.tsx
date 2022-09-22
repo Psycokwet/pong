014af48fb44dd869e5b463e3934016f8f4d5a6de
Sophie Carboni <socarboni@gmail.com>
2022-09-22 14:15:04 +0200

    fix : Naming consitency

--- frontend/src/components/PracticeJwt.tsx
+++ frontend/src/components/PracticeJwt.tsx
@@ -4,7 +4,7 @@ import { DisconnectionButton } from "./ConnectionButton/DisconnectionButton";
 import "./PracticeJwt.css";
 import { Api } from "../api/api";
 import { PictureForm } from "./PictureForm/PictureForm";
-import WebsSocketCdaiTest from "./WebSocketCdaiTest/WebsSocketCdaiTest";
+import WebSocketCdaiTest from "./WebSocketCdaiTest/WebSocketCdaiTest";
 
 //broken example component, I might even say, deprecated ;) To delete later
 function PracticeJwt() {
@@ -46,7 +46,7 @@ function PracticeJwt() {
     <>
       {/*  isConnected ? (*/}
       <div className="App">
-        <WebsSocketCdaiTest />
+        <WebSocketCdaiTest />
         {/* <div>
           <DisconnectionButton setDisconnected={() => setConnected(false)} />
         </div> */}
