5d66b6fb414ede9e19944fe803240b51ff08cdce
mescande <mescande@student.42.fr>
2022-09-23 18:41:21 +0200

    Fix <front, chat> : Post-review Changes
    Change folder name
    Change tabulation setting
    Change default messages

--- frontend/src/components/Chat/Messages/Messages.tsx
+++ frontend/src/components/Chat/Messages/Messages.tsx
@@ -2,49 +2,49 @@ import { useRef } from "react";
 import UserPicture from "../../User Picture/UserPicture";
 
 type userType = {
-	login: string;
-	nickname: string;
-//	status: userStatusEnum;
-	link_to_profile: string;
+  login: string;
+  nickname: string;
+//  status: userStatusEnum;
+  link_to_profile: string;
 }
 type MessageType = {
-	content: string;
-	sender: userType;
+  content: string;
+  sender: userType;
 }
 
 const Messages = ({messages}: {messages:MessageType[]}) => {
-	const containerRef = useRef();
-	const refAssignCallback = (ref: any) => {
+  const containerRef = useRef();
+  const refAssignCallback = (ref: any) => {
     if (!containerRef.current && ref) {
       containerRef.current = ref;
-			containerRef.current.scrollTop = containerRef.current.scrollHeight - containerRef.current.getBoundingClientRect().height;
+      containerRef.current.scrollTop = containerRef.current.scrollHeight - containerRef.current.getBoundingClientRect().height;
     } else {
       //otherwise just assign/unassigned
       containerRef.current = ref;
     }
   };
 
-	return (
-		<div 
-			ref={refAssignCallback}
-			className="row-span-5 col-span-3 scroll-smooth overflow-y-auto">
-			{messages.map((message, index) => {
-			return (
-				<div key={index} className="flex gap-3 py-4 px-10">
-					<UserPicture width="50px"/>
-					<div className="">
-						<h6 className="text-lg font-semibold self-center">
-							{message.sender.nickname}
-						</h6>
-						<p className="break-all max-w-full whitespace-pre-wrap text-sm">
-							{message.content}
-						</p>
-					</div>
-				</div>
-			)
-		})}
-		</div>
-	)
+  return (
+    <div 
+      ref={refAssignCallback}
+      className="row-span-5 col-span-3 scroll-smooth overflow-y-auto">
+      {messages.map((message, index) => {
+      return (
+        <div key={index} className="flex gap-3 py-4 px-10">
+          <UserPicture width="50px"/>
+          <div className="">
+            <h6 className="text-lg font-semibold self-center">
+              {message.sender.nickname}
+            </h6>
+            <p className="break-all max-w-full whitespace-pre-wrap text-sm">
+              {message.content}
+            </p>
+          </div>
+        </div>
+      )
+    })}
+    </div>
+  )
 }
 
 export default Messages
