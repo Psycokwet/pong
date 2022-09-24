3afa88fc13f551e2edbe3c652f470ada0a1a1d62
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-24 21:09:12 +0200

    update, sign in page, front: add simple style

--- frontend/src/components/SignInPage/SignInPage.tsx
+++ frontend/src/components/SignInPage/SignInPage.tsx
@@ -10,11 +10,11 @@ const SignInPage = () => {
   const [nickname, setNickName] = useState("thi-nguy"); // Todo : get 42login and put it as initial value
   const [selectedFile, setSelectedFile] = useState<File>();
   const [avatar, setAvatar] = useState("");
-  const [twoFactor, setTwoFactor] = useState("false");
+  const [twoFactor, setTwoFactor] = useState("off");
 
   const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
     const api = new Api();
-    if (event === undefined) return
+    if (event === undefined) return;
     event.preventDefault();
 
     const fileData = new FormData();
@@ -26,23 +26,23 @@ const SignInPage = () => {
         else console.log(`Set picture is ok, status is: ${res.status}`);
       });
     }
-    
-    if (nickname.length <= MAX_CHAR)
-    {
+
+    if (nickname.length <= MAX_CHAR) {
       api.set_nickname(nickname).then((res: Response) => {
         console.log("set_nickname is ok", res);
         if (!(res.status / 200 >= 1 && res.status / 200 <= 2))
-        res.json().then((content) => {
-          console.log("set_nickname", content);
-        });
+          res.json().then((content) => {
+            console.log("set_nickname", content);
+          });
       });
     }
 
     // Todo next: set 2Factor through api.
+    console.log(`twoFactor status is set to: ${twoFactor}`);
   };
 
   const handlePreviewPhoto = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!e.target.files) return
+    if (!e.target.files) return;
     const file = e.target.files[0];
     const fileURL = URL.createObjectURL(file);
     setAvatar(fileURL);
@@ -57,7 +57,7 @@ const SignInPage = () => {
 
   return (
     <div>
-      <div>
+      <div className="bg-gray-900">
         {avatar ? (
           <img src={avatar} className="w-40 rounded-full" />
         ) : (
@@ -74,17 +74,17 @@ const SignInPage = () => {
         onSubmit={handleSubmitForm}
       >
         {/* Should I add something to pre-display the photo? */}
-        <h1>Choose your avatar</h1>
+        <h1>Change your avatar</h1>
         <input type="file" onChange={handlePreviewPhoto} />
 
-        <h1>Choose your nickname</h1>
+        <h1>Change your nickname</h1>
         <input
           type="text"
           name="nickname"
           value={nickname}
           onChange={(e) => setNickName(e.target.value)}
-          placeholder="Choose your nickname"
-          className="text-gray-900 placeholder:text-gray-400 outline_none rounded-xl"
+          placeholder={`name less than ${MAX_CHAR} letters`}
+          className="text-gray-900 placeholder:text-gray-400 placeholder:px-4 outline_none rounded-xl w-60"
         />
         {nickname.length > MAX_CHAR ? (
           <label className="text-yellow-400">
@@ -93,19 +93,23 @@ const SignInPage = () => {
         ) : (
           ""
         )}
+        <div className="flex flex-row">
+          <span>Activate Two-Factor Authentication</span>
+          <input
+            type="checkbox"
+            onChange={(e) => setTwoFactor(e.target.value)}
+            className="text-gray-800 mx-4"
+          >
+          </input>
+        </div>
 
-        <span>Activate Two-Factor Authentication</span>
-        <select
-          value={twoFactor}
-          onChange={(e) => setTwoFactor(e.target.value)}
-          className="text-gray-800"
+        <button
+          type="submit"
+          className="w-fit bg-sky-500 hover:bg-sky-700 text-xl rounded-3xl p-4 shadow-md shadow-blue-500/50"
         >
-          <option value="false">false</option>
-          <option value="true">true</option>
-        </select>
-
-        <button type="submit"> Submit all</button>
-
+          {""}
+          Submit
+        </button>
       </form>
 
       {/* **********Testing Zone - to delete later ************* */}
