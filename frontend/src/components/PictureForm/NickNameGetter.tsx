98489bf7344e3cd81b440b1ee7d399d7605c55ac
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-24 19:57:36 +0200

    feat, sign in page, front: user can not update too long nickname

--- frontend/src/components/PictureForm/NickNameGetter.tsx
+++ frontend/src/components/PictureForm/NickNameGetter.tsx
@@ -7,7 +7,7 @@ const NickNameGetter = () => {
 
   const api = new Api();
 
-  const handleCick = (e) => {
+  const handleCick = (e: Event) => {
     e.preventDefault();
 
     api.get_nickname("thi-nguy").then((res: Response) => {
@@ -15,14 +15,19 @@ const NickNameGetter = () => {
         res.json().then((content) => {
           console.log("get_nickname is ok, result is: ", content);
         });
-        setUserNickName("  something !!!")
+        setUserNickName("  something !!!");
       } else console.log(res.status);
     });
   };
 
   return (
     <div>
-      <button onClick={handleCick}>View Uploaded NickName</button>
+      <button
+        onClick={handleCick}
+        className="bg-sky-500 hover:bg-sky-700 text-3xl rounded-3xl p-4 shadow-md shadow-blue-500/50"
+      >
+        View Uploaded NickName:{" "}
+      </button>
       <div>Nickname is set to: {userNickname}</div>
     </div>
   );
--- frontend/src/components/SignInPage/SignInPage.tsx
+++ frontend/src/components/SignInPage/SignInPage.tsx
@@ -4,84 +4,69 @@ import { Api } from "../../api/api";
 import { PictureGetter } from "../PictureForm/PictureGetter";
 import NickNameGetter from "../PictureForm/NickNameGetter";
 
-const MAX_CHAR = 20;
-const DEFAULT_PHOTO = "abc";
+const MAX_CHAR = 5;
 
 const SignInPage = () => {
   const [nickname, setNickName] = useState("thi-nguy"); // Todo : get 42login and put it as initial value
-  const [selectedPhoto, setPhoto] = useState<File>();
+  const [selectedFile, setSelectedFile] = useState<File>();
+  const [avatar, setAvatar] = useState("");
   const [twoFactor, setTwoFactor] = useState("false");
-  const [avatar, setAvatar] = useState();
 
   const handleSubmitForm = (event: Event) => {
     const api = new Api();
     event.preventDefault();
 
     const fileData = new FormData();
-    if (selectedPhoto) {
-      fileData.append("file", selectedPhoto);
+    if (selectedFile) {
+      fileData.append("file", selectedFile);
       api.setPicture(fileData).then((res: Response) => {
         if (!(res.status / 200 >= 1 && res.status / 200 <= 2))
           console.log("set picture is not Ok");
         else console.log(`Set picture is ok, status is: ${res.status}`);
       });
     }
-
-    api.set_nickname(nickname).then((res: Response) => {
-      console.log("set_nickname is ok", res);
-      if (!(res.status / 200 >= 1 && res.status / 200 <= 2))
+    
+    if (nickname.length <= MAX_CHAR)
+    {
+      api.set_nickname(nickname).then((res: Response) => {
+        console.log("set_nickname is ok", res);
+        if (!(res.status / 200 >= 1 && res.status / 200 <= 2))
         res.json().then((content) => {
           console.log("set_nickname", content);
         });
-    });
+      });
+    }
 
     // Todo next: set 2Factor through api.
   };
 
   const handlePreviewPhoto = (e: Event) => {
     const file = e.target.files[0];
-
-    file.preview = URL.createObjectURL(file);
-    setAvatar(file);
-    setPhoto(file);
+    const fileURL = URL.createObjectURL(file);
+    setAvatar(fileURL);
+    setSelectedFile(file);
   };
 
   useEffect(() => {
     return () => {
-      avatar && URL.revokeObjectURL(avatar.preview);
+      avatar && URL.revokeObjectURL(avatar);
     };
   }, [avatar]);
 
-  /****************************************************
-   *      TESTING ZONE - open Console
-   * For testing in the console, to delete later
-   * Notes: it gets the current nickname, not the newly set one)
-   ****************************************************/
-  // const getsetNickname = (event: Event) => {
-  //   const api = new Api();
-  //   event.preventDefault();
-  //   api.get_nickname(nickname).then((res: Response) => {
-  //     if (!(res.status / 200 >= 1 && res.status / 200 <= 2))
-  //       res.json().then((content) => {
-  //         console.log("get_nickname is ok, result is: ", content);
-  //       });
-  //   });
-  // };
-  /******************************************************************* */
-
   return (
     <div>
-      {/* ******************* * testing Zone ****************** */}
       <div>
-        <PictureGetter />
-        <NickNameGetter />
+        {avatar ? (
+          <img src={avatar} className="w-40 rounded-full" />
+        ) : (
+          <img
+            src="https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg"
+            alt="Preview selected photo"
+            className="w-40 rounded-full"
+          />
+        )}
       </div>
-      {/* ****************************************************** */}
 
-      <div>
-        <div>Selected photo</div>
-        {avatar && <img src={avatar.preview} className="w-80 rounded-full" />}
-      </div>
       <form
         className="text-white bg-gray-900 h-screen flex flex-col"
         onSubmit={handleSubmitForm}
@@ -119,9 +104,14 @@ const SignInPage = () => {
 
         <button type="submit"> Submit all</button>
 
-        <p>{nickname}</p>
-        <p>{twoFactor}</p>
       </form>
+
+      {/* **********Testing Zone - to delete later ************* */}
+      <div>
+        <PictureGetter />
+        <NickNameGetter />
+      </div>
+      {/* ****************************************************** */}
     </div>
   );
 };
