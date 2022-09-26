ae76cdac3379af0f443becb41938b9164335ea10
Dai Clement <cdai@student.42.fr>
2022-09-26 13:26:06 +0200

    feat [general][game step]: add redirect from gameOver to other screen

--- frontend/src/components/PongGame/GameOver.tsx
+++ frontend/src/components/PongGame/GameOver.tsx
@@ -5,9 +5,47 @@ import GameRoom from "/shared/interfaces/GameRoom";
 import { ROUTES_BASE } from "/shared/websocketRoutes/routes";
 import myConfig from '../../myConfig';
 
-const GameOver = () => {
+const GameOver = ({
+    socket,
+    gameRoom,
+    setStep,
+  }:
+  {
+    socket: Socket,
+    gameRoom: GameRoom,
+    setStep: any,
+  }
+) => {
+
+  const getWinner = () => {
+    return gameRoom.gameData.player1.score > gameRoom.gameData.player2.score ?
+      gameRoom.gameData.player1.pongUsername
+      :
+      gameRoom.gameData.player2.pongUsername
+  }
+  const joinNewGame = () => {
+      /** GAME JOIN */
+    socket?.emit(ROUTES_BASE.GAME.JOIN_GAME_REQUEST);
+    setStep(1);
+  /** END GAME JOIN */
+  }
+  const redirectToLobby = () => {
+    setStep(0);
+  }
   return <div>
-    GameOver
+    <p>
+      GameOver
+    </p>
+    <p>
+      {getWinner()} WINS
+    </p>
+    <button onClick={joinNewGame}>
+      Join a new game
+    </button>
+    <div></div>
+    <button onClick={redirectToLobby}>
+      Go to GameLobby
+    </button>
   </div>
 } 
 
