9793a8b210cee45717d807c3d556d2525a783d10
NelMasr <nel-masr@e1r12p6.clusters.42paris.fr>
2022-09-27 19:53:20 +0200

    feat <back> <userWebsocket>: see body
    User websockets are now stored in a user gateway file.
    addFriends and friendsList are now obtained through websocket.

--- backend/src/user/user.gateway.ts
+++ backend/src/user/user.gateway.ts
@@ -1,15 +1,20 @@
-import { forwardRef, Inject } from '@nestjs/common';
+import { forwardRef, Inject, UseGuards } from '@nestjs/common';
 import {
   ConnectedSocket,
+  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'http';
+import { Server } from 'socket.io';
+import { ROUTES_BASE } from 'shared/websocketRoutes/routes';
 import { UsersWebsockets } from 'shared/interfaces/UserWebsockets';
 import { Socket } from 'socket.io';
+import { JwtWsGuard, UserPayload } from 'src/auth/jwt-ws.guard';
 import { UsersService } from './user.service';
+import AddFriend from 'shared/interfaces/AddFriend';
 
 @WebSocketGateway({
   transport: ['websocket'],
@@ -18,7 +23,7 @@ import { UsersService } from './user.service';
 export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     @Inject(forwardRef(() => UsersService))
-    private readonly userService: UsersService
+    private readonly userService: UsersService,
   ) {}
   public static userWebsockets: UsersWebsockets[] = [];
 
@@ -28,7 +33,6 @@ export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
     try {
       const user = await this.userService.getUserFromSocket(client);
 
-      console.log(`New user connection: ${user.id}`);
       if (!user) return;
       const isRegistered = UserGateway.userWebsockets.find(
         (element) => element.userId === user.id,
@@ -40,7 +44,6 @@ export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
           ...UserGateway.userWebsockets,
           newWebsocket,
         ];
-        console.log(UserGateway.userWebsockets);
       }
     } catch (e) {
       console.error(e.message);
@@ -51,5 +54,41 @@ export class UserGateway implements OnGatewayConnection, OnGatewayDisconnect {
     UserGateway.userWebsockets = UserGateway.userWebsockets.filter(
       (websocket) => websocket.socketId !== client.id,
     );
+    console.log(UserGateway.userWebsockets);
+  }
+
+  @UseGuards(JwtWsGuard)
+  @SubscribeMessage(ROUTES_BASE.USER.ADD_FRIEND_REQUEST)
+  async addFriend(
+    @MessageBody() friendToAdd: AddFriend,
+    @ConnectedSocket() client: Socket,
+    @UserPayload() payload: any,
+  ) {
+    const friend = await this.userService.findOneByPongUsername(
+      friendToAdd.pongUsername,
+    );
+    const caller = await this.userService.getById(payload.userId);
+
+    await this.userService.addFriend(friend, caller);
+
+    this.server.in(client.id).emit(ROUTES_BASE.USER.ADD_FRIEND_CONFIRMATION, {
+      newFriend: friendToAdd.pongUsername,
+    });
+  }
+
+  @UseGuards(JwtWsGuard)
+  @SubscribeMessage(ROUTES_BASE.USER.FRIEND_LIST_REQUEST)
+  async friendList(
+    @MessageBody() userId: number,
+    @ConnectedSocket() client: Socket,
+    @UserPayload() payload: any,
+  ) {
+    const caller = await this.userService.getById(/*payload.*/ userId);
+
+    const orderedFriendsList = await this.userService.getFriendsList(caller);
+
+    this.server
+      .in(client.id)
+      .emit(ROUTES_BASE.USER.FRIEND_LIST_CONFIRMATION, orderedFriendsList);
   }
 }
