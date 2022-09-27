90b41edcb148c4e3da95202e3fa388d41ced1d9d
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-27 09:43:04 +0200

    feat, navbar, front: add api call login42 to NavBar (profile page)

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Api } from "../../api/api";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // Components
 import APage from "./APage";
@@ -14,60 +14,61 @@ import { RiPingPongFill } from "react-icons/ri";
 import { IoMdSettings } from "react-icons/io";
 import { DisconnectionButton } from "../ConnectionButton/DisconnectionButton";
 
-let connected_user = "scarboni";
-const api = new Api();
-api.get_login42().then((res: Response) => {
-  if ((res.status / 200 >= 1 && res.status / 200 <= 2))
-    res.json().then((content) => {
-      console.log("get_login42 is ok", content);
-      // Put connected_user here
-    });
-    else
-      console.log("get_login42 is NOT ok. Response is: ", res);
-});
-/***************** List of Pages ******************************************/
-export const NavBarPageList = [
-  {
-    url: "/play",
-    pageName: "play",
-    pageIcon: <RiPingPongFill size="28" />,
-  },
-  {
-    url: "/leaderboard",
-    pageName: "leader board",
-    pageIcon: <HiChartBar size="28" />,
-  },
-  {
-    url: "/community",
-    pageName: "community",
-    pageIcon: <FaComments size="28" />,
-  },
-  {
-    url: `/profile/${connected_user}`, // to change with real user
-    pageName: `profile ${connected_user}`, // to change with real user
-    pageIcon: <FaUser size="26" />,
-  },
-  {
-    url: "/settings",
-    pageName: "settings",
-    pageIcon: <IoMdSettings size="26" />,
-  },
-  {
-    url: "/practice",
-    pageName: "practice",
-    pageIcon: <IoMdSettings size="26" />,
-  },
-];
-
 type DisconnectionButtonProps = {
   setDisconnected: CallableFunction;
 };
 
+const api = new Api();
+
 /***************** Component NavBar ******************************************/
 const NavBar: React.FC<DisconnectionButtonProps> = ({ setDisconnected }) => {
+
+  const [connected_user, setConnectedUser] = useState("");
+  api.get_login42().then((res: Response) => {
+    if ((res.status / 200 >= 1 && res.status / 200 <= 2))
+    res.json().then((content) => {
+      setConnectedUser(content.login42)
+    });
+    else
+    console.log("get_login42 is NOT ok. Response is: ", res);
+  });
+  
+  /***************** List of Pages ******************************************/
+  const NavBarPageList = [
+    {
+      url: "/play",
+      pageName: "play",
+      pageIcon: <RiPingPongFill size="28" />,
+    },
+    {
+      url: "/leaderboard",
+      pageName: "leader board",
+      pageIcon: <HiChartBar size="28" />,
+    },
+    {
+      url: "/community",
+      pageName: "community",
+      pageIcon: <FaComments size="28" />,
+    },
+    {
+      url: `/profile/${connected_user}`,
+      pageName: `profile ${connected_user}`,
+      pageIcon: <FaUser size="26" />,
+    },
+    {
+      url: "/settings",
+      pageName: "settings",
+      pageIcon: <IoMdSettings size="26" />,
+    },
+    {
+      url: "/practice",
+      pageName: "practice",
+      pageIcon: <IoMdSettings size="26" />,
+    },
+  ];
   return (
     <div>
-      <nav className="border-b-8 border-sky-600 text-white h-1/8 h-min">
+      <nav className="border-b-8 border-sky-600 text-white h-min">
         <ul className="flex p-6 uppercase bg-neutral-900">
           <li>
             <Link to="/" className="text-3xl font-bold ">
