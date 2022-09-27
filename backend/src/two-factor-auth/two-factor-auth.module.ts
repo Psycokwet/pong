ad97a2f4d1e547990d39d89af9bfb5668767b31e
Sophie Carboni <socarboni@gmail.com>
2022-09-27 11:20:01 +0200

    feat : 2fa partially active

--- backend/src/two-factor-auth/two-factor-auth.module.ts
+++ backend/src/two-factor-auth/two-factor-auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from 'src/user/user.module';
 import { TwoFactorAuthController } from './two-factor-auth.controller';
 import { TwoFactorAuthService } from './two-factor-auth.service';
+import { TwoFactorAuthStrategy } from './two-factor-auth.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [TwoFactorAuthController],
-  providers: [TwoFactorAuthService],
+  providers: [TwoFactorAuthService, TwoFactorAuthStrategy],
 })
 export class TwoFactorAuthModule {}
