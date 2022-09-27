4a5fb00582a460a9cdb32f58f06a135c9fe4d09d
Sophie Carboni <socarboni@gmail.com>
2022-09-27 09:41:05 +0200

    feat : prepare 2fa new payload

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { User } from '../user/user.entity';
 import { jwtConstants } from './constants';
 export interface TokenPayload {
   userId: number;
+  is2FAAuthenticated: boolean;
 }
 @Injectable()
 export class AuthService {
@@ -28,8 +29,11 @@ export class AuthService {
     return { userId: user.id, login42: login42 };
   }
 
-  public getCookieWithJwtAccessToken(userId: number) {
-    const payload: TokenPayload = { userId };
+  public getCookieWithJwtAccessToken(
+    userId: number,
+    is2FAAuthenticated: boolean,
+  ) {
+    const payload: TokenPayload = { userId, is2FAAuthenticated };
     const token = this.jwtService.sign(payload, {
       secret: jwtConstants.JWT_ACCESS_TOKEN_SECRET,
       expiresIn: jwtConstants.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
@@ -37,8 +41,8 @@ export class AuthService {
     return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${jwtConstants.JWT_ACCESS_TOKEN_EXPIRATION_TIME}`;
   }
 
-  public getJwtRefreshToken(userId: number) {
-    const payload: TokenPayload = { userId };
+  public getJwtRefreshToken(userId: number, is2FAAuthenticated: boolean) {
+    const payload: TokenPayload = { userId, is2FAAuthenticated };
     return this.jwtService.sign(payload, {
       secret: jwtConstants.JWT_REFRESH_TOKEN_SECRET,
       expiresIn: jwtConstants.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
