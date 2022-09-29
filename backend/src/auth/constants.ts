adb91664d44173336f0e138ad1f724efcb96bd8c
Dai Clement <cdai@student.42.fr>
2022-09-29 12:17:40 +0200

    fix [general][game]: handle websocket access token expiration

--- backend/src/auth/constants.ts
+++ backend/src/auth/constants.ts
@@ -1,6 +1,6 @@
 export const jwtConstants = {
   JWT_ACCESS_TOKEN_SECRET: 'VeryComplicatePassword',
-  JWT_ACCESS_TOKEN_EXPIRATION_TIME: 900, 
+  JWT_ACCESS_TOKEN_EXPIRATION_TIME: 900,
   JWT_REFRESH_TOKEN_SECRET: 'VeryComplicatePassword',
   JWT_REFRESH_TOKEN_EXPIRATION_TIME: 86400, // 1 week
 };
