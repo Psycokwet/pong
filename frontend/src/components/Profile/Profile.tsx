9eb2555a8521dd061c8db109fc7f4aee5a5c49fe
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-30 16:28:53 +0200

    feat, profiles, fusion: Transfer old work to this branch

--- frontend/src/components/Profile/Profile.tsx
+++ frontend/src/components/Profile/Profile.tsx
@@ -1,21 +1,55 @@
-import React from "react";
-import "./Profile.css";
-
+import React, { useState, useEffect } from "react";
+import { Api } from "../../api/api";
 import { useParams } from "react-router-dom";
-import ProfileName from "./ProfileName";
+
+import UserProfile from "shared/interfaces/UserProfile";
 import OneUserProfile from "./OneUserProfile";
 
+const api = new Api();
+
 const Profile = () => {
-  const { user_login } = useParams();
+  const [userProfile, setUserProfile] = useState<UserProfile>({
+    pongUsername: "anonymous",
+    userRank: { level: NaN, userRank: NaN },
+    userHistory: {
+      nbGames: NaN,
+      nbWins: NaN,
+      games: [],
+    },
+    profilePicture: null,
+  });
+
+  const [avatarUrl, setAvatarUrl] = useState("");
+
+  const { pongUsername } = useParams();
+
+  useEffect(() => {
+    api.get_user_profile(pongUsername).then((res: Response) => {
+      console.log("get_user_profile", res);
+      res.json().then((content) => {
+        console.log(
+          `get_user_profile is OK with pongUsername = ${pongUsername}`,
+          content
+        );
+        setUserProfile(content);
+      });
+    });
+
+    api.getPicture().then((res) => {
+      if (res.status == 200)
+        res.blob().then((myBlob) => {
+          setAvatarUrl((current) => {
+            console.log(`get_picture is Ok with pongUsername = ${pongUsername}`);
+            if (current) URL.revokeObjectURL(current);
+            return URL.createObjectURL(myBlob);
+          });
+        });
+    });
+  }, [pongUsername]);
 
   return (
-    <div className="bg-black text-white h-screen flex grid grid-cols-10 grid-rows-6 gap-8">
-      <div className="col-start-2 col-span-3 row-start-2">
-        <ProfileName nickname={user_login} />
-      </div>
-
-      {/* This part will change according to each user. Need to look into how to pass data */}
-      <OneUserProfile nickname={user_login} />
+    <div className="bg-black text-white h-screen">
+      <OneUserProfile userProfile={userProfile} avatarUrl={avatarUrl} />
     </div>
   );
 };
