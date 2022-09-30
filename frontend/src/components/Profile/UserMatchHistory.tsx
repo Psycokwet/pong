99c0e81dea043be51cf73c051dda82b03e92c04d
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-09-30 16:28:53 +0200

    feat, profiles, fusion: Transfer old work to this branch

---
+++ frontend/src/components/Profile/UserMatchHistory.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const MAX_SHOWN_GAME = 4;
+
+type UserMatchHistoryProps = {
+  userHistory: {
+    time: string;
+    opponent: string;
+    winner: string;
+    id: Number;
+  }[];
+};
+
+const UserMatchHistory: React.FC<UserMatchHistoryProps> = ({ userHistory }) => {
+  return (
+    <table className="table-auto border-separate border-spacing-2">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Opponent</th>
+          <th>Winner</th>
+        </tr>
+      </thead>
+      <tbody>
+      {userHistory.map((oneGame, id) => {
+        while (id <= MAX_SHOWN_GAME)
+        return (
+          <tr>
+              <td>{oneGame.time}</td>
+              <td>{oneGame.opponent}</td>
+              <td>{oneGame.winner}</td>
+            </tr>
+          );
+        })}
+        </tbody>
+    </table>
+  );
+};
+
+export default UserMatchHistory;
