a5282bc8e9cb85111fddf2397f44977f94a630ad
Dai Clement <cdai@student.42.fr>
2022-10-01 21:48:25 +0200

    feat [general][chat]: handle privileges on password changes
    + update ChannelData to add privileges
    + new interface ChangeChannelData
    + wording
    + typing
    + stopPropagation

--- frontend/src/components/Chat/ChatList/Channel/Channel.tsx
+++ frontend/src/components/Chat/ChatList/Channel/Channel.tsx
@@ -2,52 +2,53 @@ import { Socket } from "socket.io-client";
 import { ROUTES_BASE } from "/shared/websocketRoutes/routes";
 import { ChannelData } from "/shared/interfaces/ChannelData";
 import { KeyboardEvent, useState, useEffect } from 'react';
-
+import { ChangeChannelData } from "/shared/interfaces/ChangeChannelData";
+import { Privileges } from '/shared/interfaces/UserPrivilegesEnum';
 
 const Channel = function ({channel, socket, connectedChannel}:{
   channel:ChannelData,
   socket:Socket|undefined,
   connectedChannel:ChannelData|undefined,
 }){
-  const [visible, toggleMenu] = useState<boolean>(false);
-  const [newChanPassword, setNewChanPassword] = useState<string>("");
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [newChanPassword, setNewChanPassword] = useState<string>('');
+
   useEffect(() => {
     if (connectedChannel?.channelId !== channel.channelId)
-      toggleMenu(false);
+      setIsVisible(false);
   }, [connectedChannel]);
   const handleClick = () => {
-    if (connectedChannel?.channelId !== channel.channelId) {
+    if (connectedChannel?.channelId !== channel.channelId)
       socket?.emit(ROUTES_BASE.CHAT.JOIN_CHANNEL_REQUEST, {roomId: channel.channelId});
-    }
-    else {
-      toggleMenu((val)=>!val);
-    }
+    else setIsVisible(val => !val);
   }
   const handleKeyDownPassword = (e: KeyboardEvent) => {
     if (e.code == 'Enter')
       handleClick();
   }
   const changePassword = () => {
-    socket?.emit(ROUTES_BASE.CHAT.CHANGE_PASSWORD_REQUEST, {channelName: channel.channelName, inputPassword:newChanPassword});
+    socket?.emit(ROUTES_BASE.CHAT.CHANGE_PASSWORD_REQUEST, {
+      channelName: channel.channelName,
+      inputPassword:newChanPassword
+    } as ChangeChannelData);
     setNewChanPassword('');
-    toggleMenu(false);
+    setIsVisible(false);
   }
   return (
     <div
-      className={("max-w-full truncate text-lg font-semibold self-center py-4 px-10 hover:bg-slate-800 cursor-pointer overflow-visible"+ ((connectedChannel !== undefined && connectedChannel.channelId === channel.channelId) ? ' bg-slate-600':''))}
+      className={("max-w-full truncate text-lg font-semibold self-center py-4 px-10 hover:bg-slate-800 cursor-pointer overflow-visible" + ((connectedChannel !== undefined && connectedChannel.channelId === channel.channelId) ? ' bg-slate-600':''))}
       onClick={handleClick}>
       <p>{channel.channelName}</p>
-      {visible ?
+      {isVisible && connectedChannel.currentUserPrivileges === Privileges.OWNER ?
         <div className="flex flex-col gap-2">
           <input
             className="bg-slate-600"
             type="text"
             autoFocus={true}
-            placeholder="Change Password"
+            placeholder="Change password"
             value={newChanPassword}
-            onChange={(e) => {
-              setNewChanPassword(e.target.value);
-            }}
+            onClick={(e) => e.stopPropagation()}
+            onChange={(e) => setNewChanPassword(e.target.value)}
             onKeyDown={handleKeyDownPassword}
           ></input>
           <button onClick={changePassword}>Validate</button>
