85a36788e2504d3cffcd3f1f7c50f06bfcc34e28
Sophie Carboni <socarboni@gmail.com>
2022-10-01 16:33:13 +0200

    feat : mockup 2 fa sign in

--- frontend/src/components/Mockup/TwoStepSigningMockup.tsx
+++ frontend/src/components/Mockup/TwoStepSigningMockup.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Api } from "../../api/api";
+
+const api = new Api();
 
 const TwoStepSigningMockup = () => {
+  const [code2fa, setCode2fa] = useState<string>("");
   return (
-    <div className="flex flex-col justify-center items-center space-y-16 bg-gray-900 text-white h-7/8 text-center">
-      <h1 className="font-bold text-6xl h-auto py-8  animate-pulse">
-        Page is loading ...
-      </h1>
-      <div>
-        <div className="border-4 p-40 border-solid rounded-3xl w-full">
-          <img
-            src="https://media4.giphy.com/media/xThuWtNFKZWG6fUFe8/giphy.gif"
-            alt="funny animation GIF"
-          />
-        </div>
-      </div>
-    </div>
+    <>
+      <input
+        value={code2fa}
+        onChange={(e) => setCode2fa(e.target.value)}
+      ></input>
+      <button
+        className="bg-sky-500 hover:bg-sky-700 text-3xl rounded-3xl p-4 shadow-md shadow-blue-500/50"
+        onClick={() => {
+          api.check_2fa(code2fa).then((res: Response) => {
+            console.log("turn_on_2fa", res);
+            res.json().then((content) => {
+              console.log("turn_on_2fa", content);
+            });
+          });
+        }}
+      >
+        turn on 2fa
+      </button>
+    </>
   );
 };
 
