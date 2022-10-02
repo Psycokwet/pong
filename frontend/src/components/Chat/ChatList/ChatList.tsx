d73d856eceebbd213ff96093021fc6f586d96e7d
Sophie Carboni <socarboni@gmail.com>
2022-10-02 20:56:26 +0200

    norm

--- frontend/src/components/Chat/ChatList/ChatList.tsx
+++ frontend/src/components/Chat/ChatList/ChatList.tsx
@@ -8,83 +8,91 @@ import { ROUTES_BASE } from "/shared/websocketRoutes/routes";
 import { ChannelData } from "/shared/interfaces/ChannelData";
 import { Message } from "/shared/interfaces/Message";
 
-function ChatList({ msg , socket , connectedChannel, handleLeaveChannel} : {
-    msg: Message,
-    socket:Socket | undefined,
-    connectedChannel: ChannelData | undefined,
-    handleLeaveChannel: any;
-}){
+function ChatList({
+  msg,
+  socket,
+  connectedChannel,
+  handleLeaveChannel,
+}: {
+  msg: Message;
+  socket: Socket | undefined;
+  connectedChannel: ChannelData | undefined;
+  handleLeaveChannel: any;
+}) {
   const [channelList, setChannelList] = useState<ChannelData[]>([]);
   const [directMessageList, setDirectMessageList] = useState<ChannelData[]>([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     socket?.emit(ROUTES_BASE.CHAT.JOIN_ATTACHED_CHANNEL_LOBBY_REQUEST);
   }, []);
-  useEffect(()=> {
+  useEffect(() => {
     socket?.emit(ROUTES_BASE.CHAT.JOIN_DM_CHANNEL_LOBBY_REQUEST);
   }, []);
 
   useEffect(() => {
     socket?.on(
       ROUTES_BASE.CHAT.LIST_ALL_ATTACHED_CHANNELS,
-      (newChannelList:ChannelData[]) => setChannelList(newChannelList),
+      (newChannelList: ChannelData[]) => setChannelList(newChannelList)
     );
     return () => {
       socket?.off(
         ROUTES_BASE.CHAT.LIST_ALL_ATTACHED_CHANNELS,
-        (newChannelList:ChannelData[]) => setChannelList(newChannelList)
+        (newChannelList: ChannelData[]) => setChannelList(newChannelList)
       );
     };
   }, []);
 
-  const resetDirectMessageList = (directMessageList:ChannelData[]) => {
+  const resetDirectMessageList = (directMessageList: ChannelData[]) => {
     setDirectMessageList(directMessageList);
-  }
+  };
   useEffect(() => {
     socket?.on(ROUTES_BASE.CHAT.LIST_ALL_DM_CHANNELS, resetDirectMessageList);
     return () => {
-      socket?.off(ROUTES_BASE.CHAT.LIST_ALL_DM_CHANNELS, resetDirectMessageList);
+      socket?.off(
+        ROUTES_BASE.CHAT.LIST_ALL_DM_CHANNELS,
+        resetDirectMessageList
+      );
     };
   }, [resetDirectMessageList]);
-
 
   return (
     <div className="h-full row-start-1 row-span-6 col-start-1 self-center scroll-smooth overflow-y-auto overflow-scroll scroll-pb-96 snap-y snap-end relative">
       <div>
-        <ChannelMenu socket={socket}/>
-        {
-          channelList.map((channel, i) =>
-            <div key={i}>
-              <Channel
-                handleLeaveChannel={() => {
-                  handleLeaveChannel();
-                  setChannelList(current => current.filter(channel => channel.channelId !== connectedChannel.channelId))
-                }}
-                channel={channel}
-                socket={socket}
-                connectedChannel={connectedChannel}
-              />
-            </div>
-          )
-        }
+        <ChannelMenu socket={socket} />
+        {channelList.map((channel, i) => (
+          <div key={i}>
+            <Channel
+              handleLeaveChannel={() => {
+                handleLeaveChannel();
+                setChannelList((current) =>
+                  current.filter(
+                    (channel) =>
+                      channel.channelId !== connectedChannel.channelId
+                  )
+                );
+              }}
+              channel={channel}
+              socket={socket}
+              connectedChannel={connectedChannel}
+            />
+          </div>
+        ))}
       </div>
       <div>
-        <DirectMessageMenu socket={socket}/>
-        {
-          directMessageList.map((channel) => 
-            <div key={channel.channelname}>
-              <DirectMessage
-                socket={socket}
-                channel={channel}
-                message={msg===undefined?"":msg}
-                connectedChannel={connectedChannel}
-              />
-            </div>
-        )
-        }
+        <DirectMessageMenu socket={socket} />
+        {directMessageList.map((channel) => (
+          <div key={channel.channelname}>
+            <DirectMessage
+              socket={socket}
+              channel={channel}
+              message={msg === undefined ? "" : msg}
+              connectedChannel={connectedChannel}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
-export default ChatList
+export default ChatList;
