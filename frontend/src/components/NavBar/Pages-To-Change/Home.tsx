9bd17e3a517d15550696eafef6373fb9cf670364
thi-nguy-PC <bethuikhongden2@gmail.com>
2022-10-02 18:46:16 +0200

    feat, homepage, front: new style for Hompage

--- frontend/src/components/NavBar/Pages-To-Change/Home.tsx
+++ frontend/src/components/NavBar/Pages-To-Change/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Loading from '../../Common/Loading'
 
 const Home = () => {
   return (
-    <Loading />
-  )
-}
+    <div className="flex flex-col justify-center items-center space-y-16 bg-gray-900 text-white h-screen text-center">
+      <h1 className="font-bold text-6xl h-auto py-8">Welcome!</h1>
+      <div>
+        <div className="border-4 p-40 border-solid rounded-3xl w-full">
+          <button
+            className="bg-red-500 hover:bg-red-700 text-3xl rounded-3xl p-4 shadow-md shadow-red-500/50"
+          >
+            Quick Play
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home
