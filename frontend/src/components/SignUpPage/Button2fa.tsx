e7d88e1ed61886c1dc0c5f1256a53f5fbdb6c679
mescande <mescande@student.42.fr>
2022-10-03 20:23:27 +0200

    Clean <front, settings> : remove wrong onClick Function

--- frontend/src/components/SignUpPage/Button2fa.tsx
+++ frontend/src/components/SignUpPage/Button2fa.tsx
@@ -18,9 +18,24 @@ const Button2fa = ({
     setStatus(twoFactorSteps.LOADING);
   };
   const result:JSX.Element[] = [
-      <button className="border-4 border-gray-400 bg-gray-400 hover:border-gray-300 hover:bg-gray-300 transition rounded-md w-20 h-12" onClick={validateClick}>Validate</button>,
-      <Puff stroke="#07e6ed" speed={1} className="h-12 transition w-20" onClick={()=>{setStatus(twoFactorSteps.DONE)}}/>,
-      <button disabled={true} className="w-20 h-12 border-4 border-green-600 bg-green-600 transition rounded-md">Done !</button>,
+      <button
+        className="border-4 border-gray-400 bg-gray-400 hover:border-gray-300 hover:bg-gray-300 transition rounded-md w-20 h-12"
+        onClick={validateClick}>
+          Validate
+      </button>,
+
+      <Puff 
+        stroke="#07e6ed"
+        speed={1}
+        className="h-12 transition w-20"
+      />,
+
+      <button
+        disabled={true}
+        className="w-20 h-12 border-4 border-green-600 bg-green-600 transition rounded-md">
+          Done !
+      </button>,
+
       <BsShieldFillExclamation className="w-20 h-12 transition text-red-600"/>
   ];
   return result[status];
